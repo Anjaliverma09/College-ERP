b69db552f583c7d55ab9e9ceb57572dc0fb42e67
Anjali <anjaliiverma09@gmail.com>
2026-03-30 22:22:19 +0530

    initial files

---
+++ main_app/context_processors.py
@@ -0,0 +1,6 @@
+def portfolio_projects(request):
+    """
+    Custom context processor to supply portfolio projects.
+    Returns an empty context as there are no portfolio projects configured.
+    """
+    return {}
